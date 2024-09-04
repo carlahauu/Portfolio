d96a9dbf6c857384c04b728e266036f8810d4575
carlahauu <carlahau8@gmail.com>
2024-09-04 04:43:35 -0700

    Improved navbar design for mobiel users

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,83 @@
 import React from "react";
 import "../styles/Navbar.css";
+import { useState } from "react";
 import WavingHandIcon from "@mui/icons-material/WavingHand";
 import { GitHub, LinkedIn } from "@mui/icons-material";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
+import { display } from "@mui/system";
 
 function Navbar() {
+  const [style, setStyle] = useState("");
+  const [mobileNav, setMobileNav] = useState(false);
+  const onClick = () => {
+    setMobileNav(!mobileNav);
+    if (mobileNav == true) {
+      setStyle("hidden");
+    } else {
+      setStyle("visible");
+    }
+  };
   return (
-    <nav className="navbarContainer">
-      <div className="logoContainer">
-      <WavingHandIcon className="logo" />
+    <>
+      <nav className="navbarContainer">
+        <div className="logoContainer">
+          <WavingHandIcon className="logo" />
+        </div>
+        <ul className="navbarItems">
+          <li className="home">
+            <a href="">home</a>
+          </li>
+          <li>
+            <a href="">projects</a>
+          </li>
+          <li>
+            <a href="">resume</a>
+          </li>
+          <li>
+            <a href="">about</a>
+          </li>
+          <li>
+            <a href="">contact</a>
+          </li>
+        </ul>
+        <div className="socialIcons">
+          <div className="githubIcon">
+            <a href="https://github.com/carlahauu">
+              <GitHub />
+            </a>
+          </div>
+          <div className="linkedinIcon">
+            <a href="https://www.linkedin.com/in/carla-hau/">
+              <LinkedIn />
+            </a>
+          </div>
+          <div className="menuBtn">
+            <a onClick={onClick}>
+              <MenuIcon fontSize="large" className="menuBtn" />
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div style={{ visibility: style }} className="mobileNavContainer">
+        <ul className="mobileNavItems">
+          <li className="home">
+            <a href="">home</a>
+          </li>
+          <li>
+            <a href="">projects</a>
+          </li>
+          <li>
+            <a href="">resume</a>
+          </li>
+          <li>
+            <a href="">about</a>
+          </li>
+          <li>
+            <a href="">contact</a>
+          </li>
+        </ul>
       </div>
-      <ul className="navbarItems">
-        <li className="home">
-          <a href="">home</a>
-        </li>
-        <li>
-          <a href="">projects</a>
-        </li>
-        <li>
-          <a href="">resume</a>
-        </li>
-        <li>
-          <a href="">about</a>
-        </li>
-        <li>
-          <a href="">contact</a>
-        </li>
-      </ul>
-      <div className="socialIcons">
-        <div className="githubIcon">
-          <a href="https://github.com/carlahauu"><GitHub /></a>
-        </div>
-        <div className="linkedinIcon">
-          <a href="https://www.linkedin.com/in/carla-hau/"><LinkedIn /></a>
-        </div>
-        <div className="menuBtn">
-            <MenuIcon fontSize="large" className="menuBtn" />
-        </div>
-      </div>
-    </nav>
+    </>
   );
 }
 
