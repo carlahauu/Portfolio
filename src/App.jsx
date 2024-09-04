4c214f25104915f3d71cda64f383ed012e533558
carlahauu <carlahau8@gmail.com>
2024-09-04 06:45:32 -0700

    Created Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
         </Routes>
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import { Email, GitHub, LinkedIn } from '@mui/icons-material'
+import React from 'react'
+import "../styles/Footer.css";
+
+function Footer(){
+  return (
+    <footer className='footer'>
+        <div className="footerSocials">
+          <div>
+            <a href="https://github.com/carlahauu">
+              <GitHub fontSize="large" />
+            </a>
+          </div>
+          <div>
+            <a href="https://www.linkedin.com/in/carla-hau/">
+              <LinkedIn fontSize="large" />
+            </a>
+          </div>
+          <div>
+            <a href='mailto:carlahau110@gmail.com'>
+              <Email fontSize="large"/>
+            </a>
+          </div>
+        </div>
+        <p className='author'>© Carla Hau</p>
+        <p className='lastUpdated'>Last Updated August 2024</p>
+    </footer>
+  )
+}
+
+export default Footer
