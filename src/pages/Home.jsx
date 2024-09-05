834b7b67b0c3c9f2674e53e8d351b28bbab97457
carlahauu <carlahau8@gmail.com>
2024-09-04 23:59:59 -0700

    Updated Meta Tags

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import AboutAndChat from "../components/AboutAndChat";
 import Hero from "../components/Hero";
 import ProjectSection from "../components/ProjectSection";
-import "../styles/Home.css"
+import "../styles/Home.css";
 
 function Home() {
   return (
     <div className="homeContainer">
-        <Hero />
-        <ProjectSection />
-        <AboutAndChat />
+      <head>
+        <meta charset="UTF-8" />
+        <link rel="icon" type="image/png" href="/waving_hand.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="An ambitious software developer who is passionate about continuous learning and building user-friendly applications that solve real-world problems."
+        />
+        <title>Home</title>
+      </head>
+      <Hero />
+      <ProjectSection />
+      <AboutAndChat />
     </div>
   );
 }
